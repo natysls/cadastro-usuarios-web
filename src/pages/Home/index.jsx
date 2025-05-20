6b2f2229a8a46c8389e331333725b0a512f1fbd1
Natalia <naty-aragao@hotmail.com>
2025-05-19 21:06:20 -0300

    estilizando a tela de cadastro

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,53 @@
-
-import './style.css'
+import "./style.css";
+import Trash from "../../assets/trash.svg";
 
 function Home() {
+  const users = [
+    {
+      id: "asddsf",
+      name: "Natália",
+      age: 26,
+      email: "naty@hotmail.com",
+    },
+    {
+      id: "dfgcfb",
+      name: "Alex",
+      age: 27,
+      email: "alex@hotmail.com",
+    },
+    {
+      id: "dfgomjpomkjc",
+      name: "Artur",
+      age: 22,
+      email: "artur@hotmail.com",
+    },
+  ];
 
   return (
-    <div>
-      <h1>Olá mundo!</h1>
+    <div className="container">
+      <form>
+        <h1> Cadastro de Usuários</h1>
+        <input placeholder="Nome" type="text" name="name" />
+        <input placeholder="Idade" type="number" name="age" />
+        <input placeholder="E-mail" type="text" name="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+
+      <h2>Usuário Cadastrado</h2>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Idade: <span>{user.age}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} alt="" />
+          </button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
